d4449e55c2b92addc55581cf875ea7ac44d0da16
luizgdino-arch <luizgdino@gmail.com>
2026-06-01 21:45:46 -0300

    Desafios 02

---
+++ Desafio02/media.js
@@ -0,0 +1,21 @@
+// Desafio 02 - Média da turma
+
+/*
+Crie um array com 5 notas
+- Utilize um loop para:
+     -Utilize um loop para:
+        - somar todas as notas
+        - Calcular a média
+
+    Mostre no console/output o seguinte:
+    Média da turma: 10.0
+*/
+
+let notas = [ 1, 1.5, 2, 2.5, 3]
+
+let somar = ;
+
+for(resultado = 1; resultado <= notas.lenght; resultado++ ) {
+    somar =+ notas[resultados]
+}
+
